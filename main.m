9068621d6e51d7d99cea3194b04fb4e1c26dd073
vgtlaztech <42973154+vgtlaztech@users.noreply.github.com>
2018-12-01 22:22:21 -0600

    Update main
    - Update questions in LASSO and when choosing the frequencies

--- main.m
+++ main.m
@@ -56,43 +56,123 @@ hold off
 
 % ?? Explore 
 
-% Lasso
+%% Lasso
+
+x = 2 * pi * d;
+y = a;
+d = (1:length(y))';
+% (TODO): LASSO's data needs to be regularized 
+
+% Afterwards Question ?
+% Q1: Why linear? 
+% A1: Try different polynomial term: x^n where n = 1/2, 1, 2, ...
+
+% X1: Not regularized
+% X1 = [ ones(L,1) d  d.^2 ...
+%     cos(x/365)    sin(x/365) ...
+%     cos(x/(365*10))  sin(x/(365*10)) ];
+
+% X2: Regularized
+% X2 = [ ones(L,1) d./L sqrt(d/L) ...
+%     cos(x/365)    sin(x/365) ...
+%     cos(x/(365*10))  sin(x/(365*10)) ];
+
+% X3: Our new lasso matrix :)
+% Q2: Will the constant coefficient affect the result of LASSO?
+tic
+N = 10;
+X3 = zeros(L, 2 + 2 * N);
+X3(:,1) = ones(L,1);
+X3(:,2) = d./L;
+for i = 1:N
+    X3(:, 2 + 2*i - 1) = cos(x/i);
+    X3(:, 2 + 2*i) = sin(x/i);
+end
+toc
+
+
+X = X3;
+size(X)
+
+
+w = 0; % zeros(N+2, 1);
+% w = [ 60 0 0 ...
+%     20 20 ...
+%     0.01 0.01 ]'; 
+
+tic
+lambda = 0.1;
+w = ista_solve(X, y ,w, lambda)
+toc
+
+z = X*w;
+
+hold on
+plot(d',z)
+plot(a,z)
+hold off
 
 % Ridge
 
 
-%% Part2; Method in Paper
+
+
+
+
+%% Part2.1; Method in Paper
+
 a = avetemp_data;
 T = length(a);
 
-x = [
-    52.6
-    9.95e-5
-    -20.4
-    -8.31
-    -0.197
-    0.211
-    0.992
-];
+%% Matlab Implementation
+y = avetemp_data;
 d = (1:T)';
+L = length(d);
+% Initialize x
+w = [ 60 0 20 20 0.01 0.01 ]'; 
+% (TODO): we simplify the model to exclude the variate term of solar cycle
+
+% y = x(1) + x(2) * d ...
+%          + x(3) * cos(2 *pi * d / 365.25) ...
+%          + x(4) * sin(2 *pi * d / 365.25) ...
+%          + x(5) * cos(2 *pi * d / (365.25 * 10.7862)) ...
+%          + x(6) * sin(2 *pi * d / (365.25 * 10.7862));
+
+Tseason = 365.25;
+Tsolar = 365.25 * 10.7862;
+X = [ ones(L,1) d  cos(2 * pi * d / Tseason) sin(2 * pi * d / Tseason) cos(2 * pi * d / Tsolar) sin(2 * pi * d / Tsolar) ];
+
+% Lease Square
+w_ls = inv(X' * X) * X' * y;
+
+
+
+%% Result
+d = (1:T)';
+x = [ 52.6; 9.95e-5; -20.4; -8.31; -0.197; 0.211; 0.992 ];
+% y = 52.6 + 9.95e-5 * d + (-20.4) * cos(2 * 3.14  * d / 365.25) + (-8.31) * sin(2 * 3.14  * d / 365.25) + (-0.197) * cos(0.992 * 2 * 3.14  * d / (365.25 * 10.7)) + 0.211 * sin(0.992 * 2 * 3.14  * d / (365.25 * 10.7));
 y = x(1) + x(2) * d ...
          + x(3) * cos(2 *pi * d / 365.25) ...
          + x(4) * sin(2 *pi * d / 365.25) ...
          + x(5) * cos(x(7) * 2 *pi * d / (365.25 * 10.7)) ...
-         + x(6) * sin(x(7) * 2 *pi * d / (365.25 * 10.7))
-         
-hold on
-% plot(d,a);
-figure()
-plot(d,y);
-hold off
+         + x(6) * sin(x(7) * 2 *pi * d / (365.25 * 10.7));
+     
 
+         
+     
 hold on
 figure()
-y = 52.6 + 9.95e-5 * d + (-20.4) * cos(2 * 3.14  * d / 365.25) + (-8.31) * sin(2 * 3.14  * d / 365.25) + (-0.197) * cos(0.992 * 2 * 3.14  * d / (365.25 * 10.7)) + 0.211 * sin(0.992 * 2 * 3.14  * d / (365.25 * 10.7));
+plot(d,a);;
 plot(d,y);
 hold off
 
 err = norm(y-a)
+
+
+%% 
+
+
+
+
 
 
